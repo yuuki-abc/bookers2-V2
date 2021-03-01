4e63128204af5151d557005bd51743f236c177a1
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-01 17:50:36 +0000

    アソシエーションの設定が完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
 
 	belongs_to :user
 	has_many :favorites, dependent: :destroy
+	has_many :book_comments, dependent: :destroy
 
 	def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
