ff91e5fd2d05d5aad928ccf168905b46a4a976b9
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-01 16:08:59 +0000

    アソシエーションの設定を完了

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show, :index, :edit, :update]
-  resources :books 
-
-
-
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 
 end
